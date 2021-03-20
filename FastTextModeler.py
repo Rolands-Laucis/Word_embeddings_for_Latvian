a2d8feb4790deee78c831aff04579e603ac130c6
Rolands Laucis <rolands.laucis@gmail.com>
2021-03-20 12:05:42 -0700

    Added fasttext model support

---
+++ FastTextModeler.py
@@ -0,0 +1,21 @@
+#python FastTextModeler.py
+
+import fasttext
+
+model = None
+
+def Train(model_type):
+    model = fasttext.train_unsupervised('../cleaned_lvwiki.txt', model=model_type)
+    model.save_model("../FastText_lvwiki_model.bin")
+
+def WorkWithModel(model_name):
+    model = fasttext.load_model(model_name)
+    while True:
+        txt = input("ievadi vardu: ")
+        print(model.get_nearest_neighbors(txt))
+
+#Train('skipgram')
+WorkWithModel('../FastText_lvwiki_model.bin')
+
+
+print ("Done!")
