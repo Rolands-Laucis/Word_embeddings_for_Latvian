b534f47b35ef89f70c8a5907fc3c3322b4d84fc6
Rolands Laucis <rolands.laucis@gmail.com>
2021-04-06 09:00:47 -0700

    fasttext now uses gensim for model creation

--- AnalogiesTester.py
+++ AnalogiesTester.py
@@ -1,41 +1,59 @@
 #This script tests generated word embedding model (word vector space) accuracy on a Latvian analogy data set
 
-#python AnalogiesTester.py --model_file ../Models/Word2vec_model/word2vec.wordvectors --dataset_file ../datasets/lv-analogies.txt --output_file ../datasets/results.txt
+#python AnalogiesTester.py --model_type word2vec --model_file ../Models/Word2vec_model/word2vec.wordvectors --dataset_file ../datasets/lv-analogies.txt --output_file ../datasets/results_word2vec.txt --gen_output true
+#python AnalogiesTester.py --model_type fasttext --model_file ../Models/FastText_model/FastText_combined_corp_model.wordvectors --dataset_file ../datasets/lv-analogies.txt --output_file ../datasets/results_fasttext.txt --gen_output true
+#python AnalogiesTester.py --model_type ssg --model_file ../Models/SSG_model/ssg_model.txt --dataset_file ../datasets/lv-analogies.txt --output_file ../datasets/results_ssg.txt --gen_output true
 
 import argparse
-from gensim.models import Word2Vec, KeyedVectors
+from gensim.models import Word2Vec, KeyedVectors, fasttext
 
 def main():
     #CLI argument handling
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--model_type", type=str, required=True, help="[word2vec|fasttext|ngram2vec|ssg]")
     parser.add_argument("--model_file", type=str, required=True, help="Path to the .wordvectors file.")
     parser.add_argument("--dataset_file", type=str, required=True, help="Path to the dataset txt file.")
-    parser.add_argument("--output_file", type=str, required=True, help="Path to the output txt file.")
+    parser.add_argument("--gen_output", type=bool, default=False, help="Should the output file with score and incorrect guesses be generated. Default False.")
+    parser.add_argument("--output_file", type=str, default=r'../datasets/results.txt', help="Path to the output txt file.")
     args = parser.parse_args()
 
-    #clear output file for new output data
-    output = open(args.output_file, "w", encoding='utf-8')
-    output.write("")
-    output.close()
-    print("Cleared dataset output file")
-    output = open(args.output_file, "a", encoding='utf-8')
+    if args.gen_output:
+        #clear output file for new output data
+        output = open(args.output_file, "w", encoding='utf-8')
+        output.write("")
+        output.close()
+        print("Cleared dataset output file")
+        output = open(args.output_file, "a", encoding='utf-8')
 
     #model loading
     #model = Word2Vec.load(args.model_file)
     #word_vectors = model.wv
     #del model
     #word_vectors.save("../Models/Word2vec_model/word2vec.wordvectors")
-    word_vectors = KeyedVectors.load(args.model_file, mmap='r')
+    if args.model_type == "word2vec":
+        word_vectors = KeyedVectors.load(args.model_file, mmap='r')
+    elif args.model_type == "fasttext":
+        #supposed to load the fasttext lib generated .bin file, but not enough ram
+        #word_vectors = fasttext.load_facebook_vectors(args.model_file)
+        
+        #this loads gensim FastText generated .wordvectors file
+        word_vectors = KeyedVectors.load(args.model_file, mmap='r')
+    elif args.model_type == "ssg":
+        word_vectors = KeyedVectors.load_word2vec_format(args.model_file, binary=False)
+    else:
+        print("model_type not supported!")
+        return
     print("model loaded!")
 
     #open dataset file and go through line by line
     score, sections = word_vectors.evaluate_word_analogies(args.dataset_file, case_insensitive=True)
-    output.write("Dataset evaluated with score: %1.6f. correct guesses: \n" % score)
-    for section in sections:
-        for line in section['correct']:
-            for word in line:
-                output.write("%s " % word)
-            output.write('\n')
+    if args.gen_output:
+        output.write("Dataset evaluated with score: %1.6f. incorrect guesses: \n" % score)
+        for section in sections:
+            for line in section['incorrect']:
+                for word in line:
+                    output.write("%s " % word)
+                output.write('\n')
     print("Dataset evaluated with score: %1.6f" % score)
     #print(sections)
 
@@ -55,7 +73,7 @@ def main():
             
     
     output.close()
-    print("Dataset evaluated!")
+    #print("Dataset evaluated!")
 
 if __name__ == '__main__':
     main()
--- FastTextModeler.py
+++ FastTextModeler.py
@@ -1,23 +1,31 @@
-#this script implements the fastText python library to create a word embeddings language model from my corpus using FastText method.
+#this script implements the fastText python library to create a word embeddings language model from my corpus using FastText method with gensim.
 
 #python FastTextModeler.py
 
-import fasttext
+from gensim.models import FastText
+#import fasttext
+corpus_file = r"../Cleaned_Corpora/combined_clean_corpus.txt"
 
-model = None
+model = FastText(vector_size=4, window=3, min_count=1)
+model.build_vocab(corpus_file=corpus_file)
+print("Built vocab. Starting train...")
 
-def Train(model_type):
-    model = fasttext.train_unsupervised('../cleaned_lvwiki.txt', model=model_type)
-    model.save_model("../FastText_lvwiki_model.bin")
+#train
+model.train(corpus_file=corpus_file, total_words=model.corpus_total_words, epochs=5)
 
-def WorkWithModel(model_name):
-    model = fasttext.load_model(model_name)
-    while True:
-        txt = input("ievadi vardu: ")
-        print(model.get_nearest_neighbors(txt))
+model.wv.save(r'../Models/FastText_model/FastText_combined_corp_model.wordvectors')
+#def Train(model_type):
+#    model = fasttext.train_unsupervised('../cleaned_lvwiki.txt', model=model_type)
+#    model.save_model("../FastText_lvwiki_model.bin")
+
+#def WorkWithModel(model_name):
+#    model = fasttext.load_model(model_name)
+#    while True:
+#        txt = input("ievadi vardu: ")
+#        print(model.get_nearest_neighbors(txt))
 
 #Train('skipgram')
-WorkWithModel('../FastText_lvwiki_model.bin')
+#WorkWithModel('../FastText_lvwiki_model.bin')
 
 
 print ("Done!")
