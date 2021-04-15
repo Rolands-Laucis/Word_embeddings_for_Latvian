2644807041d7b579b66ac8495e205390792dda1e
Rolands Laucis <rolands.laucis@gmail.com>
2021-04-15 16:14:06 +0300

    nlp pipe can predict UPOS and UD tag

--- SpaCy/nlp_pipe.py
+++ SpaCy/nlp_pipe.py
@@ -1,49 +1,42 @@
 #this script does NLP tasks for testing created word embeddings using python nlp SpaCy lib. For training POS and NER tagger and using them to evaluate embeddings.
 
-#On the site https://spacy.io/usage/training there is a tool to generate a base_config.cfg file, that you can download
+#On the site https://spacy.io/usage/training there is a tool to generate a base_config.cfg file, that you can download. For this the tagger was selected in the tool and for accuracy 
+#the tagger will only generate tags as token.tag_ which are fine-grained pos.
 #then that config needs to be filled in with all the other defaults. This generates the config.cfg
-#python -m spacy init fill-config base_config.cfg config.cfg
+#python -m spacy init fill-config base_config-tag.cfg config.cfg
+
+#if you want to access token.pos_ which is coarse-grained (e.g. NOUN, VERB...), you need to include a morphologizer component in the config under [components]
+#this is done by creating a template config morfo-config file and copy pasting its morphologizer component into config.cfg under [components] and "morphologizer" under [nlp]
+#spacy init config -p tagger,morphologizer,parser morfo-config.cfg
 
 #For training a model, you need a .spacy format file. To generate it, you use the covert method https://spacy.io/api/cli#convert with options
 #i converted all 3 (train, dev, test) .conllu files this way from https://github.com/UniversalDependencies/UD_Latvian-LVTB
 #python -m spacy convert lv_lvtb-ud-test.conllu .\ --converter conllu
 
-#To insert my word embeddings into spaCy, covert them to .spacy format:
-#python -m spacy init vectors lv ../../Models/SSG_model/ssg_5_200_sg.txt ../../Models/Spacy_tagger/vectors --name lv-ssg-5-200
+#To insert my word embeddings into spaCy, covert them to .spacy format. Embeddings need to be .txt with the first line speficying stats about vectors:
+#python -m spacy init vectors lv ../../Models/SSG_model/ssg_5_350_sg.txt ../../Models/Spacy_tagger/ssg_5_350_vectors --name lv-ssg-5-350
 
 #Then in the config.cfg under [Initialize] set vectors = ^ that path where it output without ""
 
+#under [training] set max_epochs = 10 for consistent results. This takes about 2h. Otherwise it says 0 and means unlimited epochs, bet eventually stops training past 100 epochs.
+
 #Train the custom model to use in the spaCy nlp pipeline for tagging later.
-#python -m spacy train config.cfg --output ../../Models/Spacy_tagger/ssg_5_200_pos --paths.train ./lv_lvtb-ud-train.spacy --paths.dev ./lv_lvtb-ud-dev.spacy
+#python -m spacy train config.cfg --output ../../Models/Spacy_tagger/ssg_5_350_pos --paths.train ./lv_lvtb-ud-train.spacy --paths.dev ./lv_lvtb-ud-dev.spacy
+
+#optionally verify config stats are in there like POS tags, embeddings...:
+#python -m spacy debug data ../../Models/Spacy_tagger/ssg_5_200_pos/model-last/config.cfg
 
 #Then run this script to do a POS tag
 #python nlp_pipe.py
 
-#from thinc.api import Config
-#from spacy.language import Language
 import spacy
-from spacy import util
-from thinc.api import Config
-from spacy.language import Language
-from spacy.lang.lv import Latvian
-from spacy.vocab import Vocab
 
-#nlp = spacy.load("../../Models/Spacy_tagger/ssg_5_200_pos/model-last/")
-config = Config().from_disk("../../Models/Spacy_tagger/ssg_5_200_pos/model-last/config.cfg")
-#nlp = Latvian()
-#language = spacy.blank("lv")
-#nlp = util.load_model_from_config(config)
-voc = Vocab(vectors_name="./vocab/vectors")
-nlp = Latvian.from_config(config, vocab=voc)
-#nlp = language.from_disk("../../Models/Spacy_tagger/ssg_5_200_pos/model-last/")
-#nlp.from_disk("../../Models/Spacy_tagger/ssg_5_200_pos/model-last/")
+nlp = spacy.load("../../Models/Spacy_tagger/ssg_5_350_pos/model-best/")
 
-#nlp.initialize(Vocab(vectors_name="./vocab/vectors"))
+#print(nlp.pipeline)
 doc = nlp("astronomija uzbūvi izvietojumu kustību un attīstību.")
 
-print(nlp.pipeline)
-
-
-#for token in doc:
+print("Word | UPOS | TAG")
+for token in doc:
     #print(token.text, token.pos)
-#    print(token.text, token.pos_)
+    print(token.text, token.pos_, token.tag_)
